9d2633f7a1b438c52120ce4d757ffa36f61d7026
home-snapseed <snapseed3151@gmail.com>
2018-12-22 15:48:54 +0600

    добавлено в gitignore auth.jsonpackage-lock.json

--- bot.js
+++ bot.js
@@ -1,30 +1,47 @@
 var Discord = require('discord.io');
+
 var logger = require('winston');
+
 var  auth = require('./auth.json');
+
 //Configure logger settings
 logger.remove(logger.transports.Console, {
+
     colorize: true
+
 });
+
 logger.level = 'debug';
+
 //Initialize Discord Bot
+
 var bot = new Discord.Client({
+
     token: auth.token,
+
     autorun: true
 });
+
 bot.on('ready',  function(evt) {
+
     logger.info('Connected');
+
     logger.info('Logged in as: ');
+
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
+
     console.log(message);
 
     var regexp = /\!thanks\b/gmi;
 
-    if (regexp.test(message) && channelID == 525249065039036426 && user !== String) {
-         bot.sendMessage({to: channelID, message:  user + " Nobody was mentioned in your message (Aliya's bot)"});
-    }
-    else if(regexp.test(message) && channelID == 525249065039036426 && user == String){
-        bot.sendMessage({to: channelID, message:  user + " thanked in message (Aliya's bot)"});
+    if(channelID == 525249065039036426) {
+
+        if (regexp.test(message)) {
+
+            bot.sendMessage({to: channelID, message:  user + " thanked in message (Aliya's bot)"});
+        }
     }
 });

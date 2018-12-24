6999c43016cd0c96e155281c22c7f9843a7dcc7c
safbek <safbek@list.ru>
2018-12-24 14:37:16 +0300

    Adding code formatting

--- bot.js
+++ bot.js
@@ -5,37 +5,36 @@ var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-	colorize: true
+  colorize: true
 });
 
 logger.level = 'debug';
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true,
+  token: auth.token,
+  autorun: true,
 });
 
 bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-	let regexp = /\!thanks/gmi;
-	let arrUsernames = [];
-	evt.d.mentions.forEach(function(item, i) {
-		arrUsernames.push(item.username);
-		return arrUsernames;  
-	})
+  let regexp = /\!thanks/gmi;
+  let arrMentionedUsers = [];
+  evt.d.mentions.forEach(function(item) {
+    arrMentionedUsers.push(item.username);
+    return arrMentionedUsers;  
+})
 
-	if (regexp.test(message)) {
-		bot.sendMessage({
-			to: channelID,
-			message: evt.d.author.username+ " поблагодарил(a) кого-то вроде (Safiya's bot)" +arrUsernames
-		});
-	}
-
+  if (regexp.test(message)) {
+    bot.sendMessage({
+      to: channelID,
+      message: evt.d.author.username+ " поблагодарил(a) пользователя(ей) "
+      +arrMentionedUsers.join(', ') +" (Safiya's bot)"
+  });
+}
 });
-

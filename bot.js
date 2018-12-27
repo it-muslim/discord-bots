814b529888d823564781ca7341636ada1c46e892
safbek <safbek@list.ru>
2018-12-27 19:08:32 +0300

    Merge master and formatting

--- bot.js
+++ bot.js
@@ -1,56 +1,60 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-  colorize: true
+    colorize: true
 });
 
 logger.level = 'debug';
 
 // Initialize Discord Bot
-var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true,
+const bot = new Discord.Client({
+    token: auth.token,
+    autorun: true,
 });
 
 bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
-/**/
+
 bot.on('message', function (user, userID, channelID, message, evt) {
-    let regexp = /\!thanks/gmi;
-    if(channelID !== '525249065039036426' ||
-     evt.d.author.id === '523928478341660702' || !(regexp.test(message)) ){
+    const regexp = /\!thanks/gmi;
+    const bot_user_id = '523928478341660702';
+    const allowed_channel = '525249065039036426';
+    const authorId = evt.d.author.id;
+    const authorName = evt.d.author.username;
+
+    if(channelID !== allowed_channel ||
+        authorId === bot_user_id || !regexp.test(message)){
         return;
-}
-let mentionedUserId = [];
-let mentionedUserNames = [];
+    }
 
-evt.d.mentions.map(function(item){
-    mentionedUserId.push(item.id);
-});
 
-evt.d.mentions.map(function(item){
-    mentionedUserNames.push(item.username);
-});
+    let mentionedUserId = evt.d.mentions.map(function(item){
+        return item.id;
+    });
 
-if (mentionedUserId.indexOf(evt.d.author.id) === -1) {
-    bot.sendMessage({
-      to: channelID,
-      message: evt.d.author.username + " поблагодарил(a) пользователя(ей) "
-      + mentionedUserNames.join(', ') + " (Safiya's bot)"
-      
-  }); 
-}else{
-    bot.sendMessage({
-      to: channelID,
-      message: "Пардон, но всему свое время и место"
-  });
-}
-console.log(message);
+    let mentionedUserNames = evt.d.mentions.map(function(item){
+        return item.username;
+    });
+
+    if (!!mentionedUserId.indexOf(authorId)) {
+        bot.sendMessage({
+            to: channelID,
+            message: authorName + " поблагодарил(a) пользователя(ей) "
+            + mentionedUserNames.join(', ') + " (Safiya's bot)"
+
+        }); 
+    }else{
+        bot.sendMessage({
+            to: channelID,
+            message: "Пардон, но всему свое время и место"
+        });
+    }
+
 });

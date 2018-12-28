3121bc0471f2e8a6ed34e750875ecacc6bdee0bf
home-snapseed <snapseed3151@gmail.com>
2018-12-28 14:26:03 +0600

    бот не реагирует, когда в сообщении не упомянуты пользователи

--- bot.js
+++ bot.js
@@ -2,17 +2,17 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var  auth = require('./auth.json');
 
-//Configure logger settings
+// Configure logger settings
 logger.remove(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
 
-//Канал, в котором бот может реагировать на сообщения
+// Канал, в котором бот может реагировать на сообщения
 const ALLOWED_CHANNEL_ID = "525249065039036426";
 
 
-//Initialize Discord Bot
+// Initialize Discord Bot
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
@@ -25,35 +25,40 @@ bot.on('ready',  function(evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    
-    var regexp = /\!thanks\b/gmi;
-
     //в каком канале пишут?
-    if(channelID !== ALLOWED_CHANNEL_ID) {
+    if (channelID !== ALLOWED_CHANNEL_ID) {
         return;
     }
 
-    //содержит нужное слово?
+    var regexp = /\!thanks\b/gmi;
+
+    // содержит нужное слово?
     if (!regexp.test(message)) {
         return;
     }
 
-    //собираю ID упомянутых пользователей  в массив
+    // собираю ID упомянутых пользователей  в массив
     var mentionedUsersID = evt.d.mentions.map(function(item, i, mentions) {
         return evt.d.mentions[i].id;
     });  
     
-    //собираю имена упомянутых пользователей в массив
+    // собираю имена упомянутых пользователей в массив
     var mentionedUsers = evt.d.mentions.map(function(item, i, mentions) {
         return evt.d.mentions[i].username;
     }); 
 
+    // если никто не упомянут в сообщении, то бот не должен отвечать
+    if (mentionedUsers.length === 0) {
+        return;
+    }
+
     var authorID = evt.d.author.id;
 
-    if(!!mentionedUsersID.indexOf(authorID)){
-        bot.sendMessage({to: channelID, message:  "не благодари самого себя :exclamation: (Aliya's bot)"})
+    // если упомянут автор сообщения, или  если упомянут пользователь
+    if (!!mentionedUsersID.indexOf(authorID)){
+        bot.sendMessage({to: channelID, message:  "не благодари самого себя :face_palm:  (Aliya's bot)"});
     } else {
-        bot.sendMessage({to: channelID, message: user + " thanked in message  :slight_smile: " + mentionedUsers + " (Aliya's bot)"})
+        bot.sendMessage({to: channelID, message: user + " thanked in message  :slight_smile: " + mentionedUsers + " (Aliya's bot)"});
     }
 });
 

0c9fddac9e48c5df65b8ebb13294b972d413feb7
safbek <safbek@list.ru>
2018-12-30 23:34:14 +0300

    Replacing the indexOf method with the includes method

--- thanks-bot/bot.js
+++ thanks-bot/bot.js
@@ -5,59 +5,56 @@ const auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    colorize: true
+	colorize: true
 });
 
 logger.level = 'debug';
 
 // Initialize Discord Bot
 const bot = new Discord.Client({
-    token: auth.token,
-    autorun: true,
+	token: auth.token,
+	autorun: true,
 });
 
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+const REGEXP = /\!thanks\b/gmi;
+const BOT_ID = '523928478341660702';
+const ALLOWED_CHANNELS = '525249065039036426';
+
 bot.on('message', function (user, userID, channelID, message, evt) {
-    const regexp = /\!thanks\b/gmi;
-    const bot_user_id = '523928478341660702';
-    const allowed_channel = '525249065039036426';
-    const authorId = evt.d.author.id;
-    const authorName = evt.d.author.username;
 
-    // Conditions for exiting a function
-    if(channelID !== allowed_channel ||
-        authorId === bot_user_id || !regexp.test(message)){
-        return;
-    }
+	// Conditions for exiting the function
+	if(channelID !== ALLOWED_CHANNELS ||
+		userID === BOT_ID || !REGEXP.test(message)) {
+		return;
+	}
 
-    // Collecting usernames
-    let mentionedUserNames = evt.d.mentions.map(item => item.username);
+	// Collecting usernames
+	let mentionedUserNames = evt.d.mentions.map(item => item.username);
 
-    // Collecting user id
-    let mentionedUserId = evt.d.mentions.map(item => item.id);
+	// Collecting user id
+	let mentionedUserId = evt.d.mentions.map(item => item.id);
 
-    // Exit function if no user is mentioned
-    if(mentionedUserId.length === 0){
-        return;
-    }
+	// Exit function if no user is mentioned
+	if(mentionedUserId.length === 0) {
+		return;
+	}
 
-    if (!!mentionedUserId.indexOf(authorId)) {
-        bot.sendMessage({
-            to: channelID,
-            message: authorName + " поблагодарил(a) пользователя(ей) "
-            + mentionedUserNames.join(', ') + " (Safiya's bot)"
-
-        }); 
-    }else{
-        bot.sendMessage({
-            to: channelID,
-            message: "Пардон, но всему свое время и место"
-        });
-    }
-
+	if (!mentionedUserId.includes(userID)) {
+		bot.sendMessage({
+			to: channelID,
+			message: user + " поблагодарил(a) пользователя(ей) "
+			+ mentionedUserNames.join(', ') + " (Safiya's bot)"
+	}); 
+	} else {
+		bot.sendMessage({
+			to: channelID,
+			message: "Нельзя благодарить самого себя"
+		});
+	}
 });

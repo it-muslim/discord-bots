b75d719bffcf26a3f0e0249dcbc7c33583ebacd8
home-snapseed <snapseed3151@gmail.com>
2018-12-31 06:35:20 +0600

    поменяла indexOf на метод includes

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var  auth = require('./auth.json');
+var auth = require('./auth.json');
 
 // Configure logger settings
 logger.remove(logger.transports.Console, {
-    colorize: true
+  colorize: true
 });
 logger.level = 'debug';
 
@@ -14,51 +14,57 @@ const ALLOWED_CHANNEL_ID = "525249065039036426";
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
+  token: auth.token,
+  autorun: true
 });
 
 bot.on('ready',  function(evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    //в каком канале пишут?
-    if (channelID !== ALLOWED_CHANNEL_ID) {
-        return;
-    }
+	// в каком канале пишут?
+	if (channelID !== ALLOWED_CHANNEL_ID) {
+		return;
+	}
 
-    var regexp = /\!thanks\b/gmi;
+	var regexp = /\!thanks\b/gmi;
 
-    // содержит нужное слово?
-    if (!regexp.test(message)) {
-        return;
-    }
+	// содержит нужное слово?
+	if (!regexp.test(message)) {
+		return;
+	}
 
-    // собираю ID упомянутых пользователей  в массив
-    var mentionedUsersID = evt.d.mentions.map(function(item, i, mentions) {
-        return evt.d.mentions[i].id;
-    });  
+  // собираю ID упомянутых пользователей  в массив
+  var mentionedUsersID = evt.d.mentions.map(function(item, i, mentions) {
+    return evt.d.mentions[i].id;
+  });  
     
-    // собираю имена упомянутых пользователей в массив
-    var mentionedUsers = evt.d.mentions.map(function(item, i, mentions) {
-        return evt.d.mentions[i].username;
-    }); 
+  // собираю имена упомянутых пользователей в массив
+  var mentionedUsers = evt.d.mentions.map(function(item, i, mentions) {
+    return evt.d.mentions[i].username;
+  }); 
 
-    // если никто не упомянут в сообщении, то бот не должен отвечать
-    if (mentionedUsers.length === 0) {
-        return;
-    }
+  // если никто не упомянут в сообщении, то бот не должен отвечать
+  if (mentionedUsers.length === 0) {
+    return;
+  }
 
-    var authorID = evt.d.author.id;
+  var authorID = evt.d.author.id;
 
-    // если упомянут автор сообщения, или  если упомянут пользователь
-    if (!!mentionedUsersID.indexOf(authorID)){
-        bot.sendMessage({to: channelID, message:  "не благодари самого себя :face_palm:  (Aliya's bot)"});
-    } else {
-        bot.sendMessage({to: channelID, message: user + " thanked in message  :slight_smile: " + mentionedUsers + " (Aliya's bot)"});
-    }
+  // если упомянут автор сообщения, или  если упомянут пользователь
+  if (mentionedUsersID.includes(authorID)){
+    bot.sendMessage({
+			to: channelID, 
+			message:  "не благодари самого себя :face_palm:  (Aliya's bot)"
+		});
+  } else {
+    bot.sendMessage({
+			to: channelID, 
+			message: user + " thanked in message  :slight_smile: " + mentionedUsers + " (Aliya's bot)"
+		});
+  }
 });
 

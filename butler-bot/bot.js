357a0e91c87ebc01308a08c9e185ba569b844d5f
safbek <safbek@list.ru>
2019-01-25 15:25:40 +0300

    Calculates the difference between dates

--- butler-bot/bot.js
+++ butler-bot/bot.js
@@ -2,14 +2,6 @@ const Discord = require('discord.js')
 const logger = require('winston')
 const auth = require('./auth.json')
 
-// Roles
-const DESCRIBE_YOURSELF_ROLE_ID = '450966946519711744'
-const FULL_MEMBER_ROLE_ID = '450967258722992129'
-
-// Channels
-const INFO_CHANNEL_ID = '441122385031200794'
-const GREETINGS_CHANNEL_ID = '444812676632412161'
-
 // Configure logger settings
 logger.remove(logger.transports.Console)
 logger.add(logger.transports.Console, {
@@ -26,41 +18,28 @@ client.on('ready', () => {
   logger.info(client.username + ' - (' + client.id + ')')
 })
 
-client.on('guildMemberAdd', (member) => {
-  member.addRole(DESCRIBE_YOURSELF_ROLE_ID)
-    .then(() => { logger.info('Successfully added the role') })
+const GREETINGS_CHANNEL_ID = '525249065039036426'
+
+client.on('message', msg => {
+  if (msg.channel.id !== GREETINGS_CHANNEL_ID || msg.author.bot) {
+    return
+  }
 
   let greetingsChannel = client.channels.get(GREETINGS_CHANNEL_ID)
-  if (typeof greetingsChannel === 'undefined') {
-    logger.error('Greetings channel is not defined')
-    return
+
+  let timeNow = new Date()
+  console.log(timeNow)
+  let timeMemberJoin = msg.member.joinedAt
+  console.log(timeMemberJoin)
+  let timeDifference = new Date(timeNow).getDate() - new Date(timeMemberJoin).getDate()
+  console.log(timeDifference)
+  if (timeDifference > 7) {
+    greetingsChannel.send(
+      `Вы скоро будете удалены из сообщества`)
+  } else {
+    greetingsChannel.send(
+      `Не забываем рассказать о себе`)
   }
-
-  let infoChannel = client.channels.get(INFO_CHANNEL_ID)
-  if (typeof infoChannel === 'undefined') {
-    logger.error('Info channel is not defined')
-    return
-  }
-
-  greetingsChannel.send(
-    `Ассаляму алейкум ва рахматуллахи ва баракатуху, ${member.toString()}!\n\n` +
-    `Добро пожаловать на IT-Muslim Дискорд сервер!\n` +
-    `Чтобы узнать побольше о сообществе, посетите канал ${infoChannel.toString()}. ` +
-    `Чтобы получить разрешение писать во всех каналах и активно yчаствовать ` +
-    `в жизни сообщества, напишите прямо сюда (${greetingsChannel.toString()}) ` +
-    `немного о себе.`
-  )
-})
-
-client.on('message', message => {
-  if (message.channel.id !== GREETINGS_CHANNEL_ID || message.author.bot) {
-    return
-  }
-
-  message.member.removeRole(DESCRIBE_YOURSELF_ROLE_ID)
-    .then(() => { logger.info('Successfully removed the role') })
-  message.member.addRole(FULL_MEMBER_ROLE_ID)
-    .then(() => { logger.info('Successfully added the role') })
 })
 
 client.login(auth.token)
